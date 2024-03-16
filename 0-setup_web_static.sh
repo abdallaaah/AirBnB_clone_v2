5ef1ef8d5a3dd0b317274f70b6462b2e131f4bdc
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-16 14:48:39 -0700

    check

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,8 +17,27 @@ sudo rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
 # nginx configuations
-sudo sed -i '/^\s*location \/hbnb_static\//d' /etc/nginx/sites-available/default
-sudo sed -i '/^\s*root\s*/s|^\s*root\s*/[^;]*|root /data/web_static/current|' /etc/nginx/sites-available/default
-sudo sed -i '/^\s*server_name _;/a\       location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 exit 0
