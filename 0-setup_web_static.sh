c68aee18098b78679c8036971e78e5dddeb71526
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-16 13:23:27 -0700

    check this task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 # this is script to configure a new ubuntu machine
-apt-get install nginx -y
+sudo apt-get install nginx -y
 mkdir -p /data/
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "hello from index nginx work " >> /data/web_static/releases/test/index.html
+sudo bash -c 'echo "hello from index nginx work " >> /data/web_static/releases/test/index.html'
 ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '58i\	location /hbnb_static/ {
-        alias /data/web_static/current/;
-        }' /etc/nginx/sites-available/default
+sudo sed -i 's|^\s*root\s*/[^;]*|root /data/web_static/current|' /etc/nginx/sites-available/default
+sudo sed -i '58i\       location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
 sudo systemctl restart nginx
-
-
-
-
 
