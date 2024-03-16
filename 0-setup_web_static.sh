ddded8ef1ec8e8f5d3a1a6af86e1da76814db8c9
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-16 13:41:00 -0700

    check the conf

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,9 +10,9 @@ sudo bash -c 'echo "hello from index nginx work " >> /data/web_static/releases/t
 sudo rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i 's|^\s*root\s*/[^;]*|root /data/web_static/current|' /etc/nginx/sites-available/default
-if ! grep -q 'location /hbnb_static/' /etc/nginx/sites-available/default; then
-    sudo sed -i '58i\       location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
-fi
+# nginx configuation
+sudo sed -i '/^\s*location \/hbnb_static\//d' /etc/nginx/sites-available/default
+sudo sed -i '/^\s*root\s*/s|^\s*root\s*/[^;]*|root /data/web_static/current|' /etc/nginx/sites-available/default
+sudo sed -i '/^\s*server_name _;/a\       location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 exit 0
