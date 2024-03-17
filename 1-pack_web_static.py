c61d1e2a8a805b6c04139d26d87e7835d0374c2e
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-16 17:29:41 -0700

    check tar task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from fabric.api import *
+import os
+from datetime import datetime
+
+def do_pack():
+
+    local("mkdir -p verions")
+    datetimenw = datetime.now().strftime("%Y%m%d%H%M%S")
+    result = local(f"tar -czvf verions/web_static_{datetimenw}.tgz web_static", capture=True)
+    if result.succeeded:
+        return os.path.abspath(f"/home/eltawil/Alx/AirBnB_clone_v2/verions/web_static_{datetimenw}.tgz")
+    else:
+        return None
