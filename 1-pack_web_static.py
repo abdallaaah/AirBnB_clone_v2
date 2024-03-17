7e4e6e946ef539d0259b6ab1a636b173e38de871
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-16 17:38:38 -0700

    check pycode style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,10 @@ def do_pack():
     """this function to create a tar file"""
     local("mkdir -p versions")
     datetimenw = datetime.now().strftime("%Y%m%d%H%M%S")
-    result = local(f"tar -czvf versions/web_static_{datetimenw}.tgz web_static", capture=True)
+    result = local(f"tar -czvf versions/web_static_{datetimenw}.tgz "
+                   f"web_static", capture=True)
     if result.succeeded:
-        return (os.path.abspath(f"/home/eltawil/Alx/AirBnB_clone_v2/versions/web_static_{datetimenw}.tgz"))
+        return (os.path.abspath(f"/home/eltawil/Alx/AirBnB_clone_v2"
+                                f"/versions/web_static_{datetimenw}.tgz"))
     else:
         return (None)
