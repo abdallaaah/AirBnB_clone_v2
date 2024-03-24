a7ea15d859f608b6a1f2145dcafa696b5a432288
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-23 21:03:31 -0700

    check markup safe

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 1. Script to start a Flask web application with 2 commands """
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """this is the index page for the app"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """this is the index page for the app"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """this is to dislay txt after the slash and replace coma with text"""
+    return f"C {escape(text.replace('_',' '))}"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text_python(text="is cool"):
+    """this is to display txt after the slash with defualt value python"""
+    return f"Python {escape(text.replace('_',' '))}"
+
+
+if __name__ == "__main__":
+    """the first point the app start from"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
