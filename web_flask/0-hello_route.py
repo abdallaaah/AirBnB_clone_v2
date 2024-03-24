0a5a0ee1cfe801542e34728d1f93831d562d831a
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-23 18:59:01 -0700

    check flask apppp

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """the entry point"""
+
 from . import app
 
 
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """this init file for the webflask app"""
+
 from flask import Flask
 
 app = Flask(__name__)
