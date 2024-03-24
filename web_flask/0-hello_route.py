87a49478faf73795f9d320dbab97a490678ceb7f
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-23 18:51:59 -0700

    check flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+from . import app
+"""the entry point"""
+
+
+if __name__ == "__main__":
+    """the first point the app start from"""
+    app.run(host='0.0.0.0', port=5000)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """this is the index page for the app"""
+    return "Hello HBNB!"
---
+++ web_flask/__init__.py
@@ -0,0 +1,4 @@
+from flask import Flask
+"""this init file for the webflask app"""
+
+app = Flask(__name__)
