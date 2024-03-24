2479939a53d1178fd14737357dee15646ac2cd1d
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-23 20:30:19 -0700

    all green

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """this is the index page for the app"""
 
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     """the first point the app start from"""
