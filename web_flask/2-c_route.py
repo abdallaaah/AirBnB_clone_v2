a59e4424b99962a69fe65b60c5829c3180db3413
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-23 20:52:38 -0700

    check flask app task 3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,4 +25,4 @@ def display_text(text):
 
 if __name__ == "__main__":
     """the first point the app start from"""
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
