4d7e763bd7e6d0f6e9346e09336d510db043a6a0
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-24 01:49:17 -0700

    check

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" 1. Script to start a Flask web application with 2 commands """
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """this is the index page for the app"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """this is the index page for the app"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """this is to dislay txt after the slash and replace coma with text"""
+    return f"C {escape(text.replace('_',' '))}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_text_python(text='is cool'):
+    """this is to display txt after the slash with defualt value python"""
+    return f"Python {escape(text.replace('_',' '))}"
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def display_integet_numbers(num):
+    """this is to display number integers"""
+    if isinstance(num, int):
+        return f"{num} is a number"
+
+
+if __name__ == "__main__":
+    """the first point the app start from"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
