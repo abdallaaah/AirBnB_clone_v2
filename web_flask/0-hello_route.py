95f844a262bf78868ff4f0dd5ecc02a5e05bcb69
abdalla <abdallah.hesham@msa.edu.eg>
2024-03-23 20:24:12 -0700

    check route one

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,20 +5,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    """the first point the app start from"""
-
-    app.run(host='0.0.0.0', port=5000)
-
-
 @app.route('/', strict_slashes=False)
 def index():
     """this is the index page for the app"""
 
     return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
-def index():
-    """this is the index page for the app"""
+if __name__ == "__main__":
+    """the first point the app start from"""
 
-    return "HBNB"
+    app.run(host='0.0.0.0', port=5000)
+
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""the entry point"""
+""" 1. Script to start a Flask web application with 2 commands """
 
 from flask import Flask
-
 app = Flask(__name__)
-
-if __name__ == "__main__":
-    """the first point the app start from"""
-
-    app.run(host='0.0.0.0', port=5000)
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """this is the index page for the app"""
-
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """this is the index page for the app"""
-
     return "HBNB"
+
+
+if __name__ == "__main__":
+    """the first point the app start from"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
